da7b8a7e308b019ecc65417e6e8201904d6ba328
Shahan Hasan <shahan.hasan101294@gmail.com>
2021-11-24 00:59:32 +0600

    GA

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 // import ReactMarkdown from 'react-markdown';
-import raw from 'raw.macro';
+// import raw from 'raw.macro';
 
 import Main from '../layouts/Main';
 
 // uses babel to load contents of file
-const markdown = raw('../data/about.md');
+// const markdown = raw('../data/about.md');
 
-const count = markdown.split(/\s+/)
-  .map((s) => s.replace(/\W/g, ''))
-  .filter((s) => s.length).length;
+// const count = markdown.split(/\s+/)
+//   .map((s) => s.replace(/\W/g, ''))
+//   .filter((s) => s.length).length;
 
 // Make all hrefs react router links
 // const LinkRenderer = ({ ...children }) => <Link {...children} />;
@@ -24,7 +24,7 @@ const About = () => (
       <header>
         <div className="title">
           <h2 data-testid="heading"><Link to="/about">About Me</Link></h2>
-          <p>(in about {count} words)</p>
+          {/* <p>(in about {count} words)</p> */}
         </div>
       </header>
       {/* <ReactMarkdown
